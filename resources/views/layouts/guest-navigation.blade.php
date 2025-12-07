d0b5f1eae78730fedf9ee7455638e53a3a4239b2
Liakat Biswas <liakatbiswas@gmail.com>
2025-12-07 09:26:06 +0600

    full width fixed

--- resources/views/layouts/guest-navigation.blade.php
+++ resources/views/layouts/guest-navigation.blade.php
@@ -1,6 +1,6 @@
 @if (Route::has('login'))
     <nav class="bg-white dark:bg-gray-800 shadow">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between h-16 items-center">
 
                 <!-- Logo / Branding -->
